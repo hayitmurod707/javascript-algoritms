72e35d9523d37f822b7e0cf9cc2a3d735c57a166
hayitmurod707 <hayitmurod707@gmail.com>
2022-08-25 19:05:52 +0500

    updated shuffle array function and added notification function

--- array/shuffle-array.js
+++ array/shuffle-array.js
@@ -8,7 +8,7 @@ const shuffleArray = array => {
 	let prevArray = [...array];
 	for (let x = 0; x < array.length; x++) {
 		maxSize = maxSize - 1;
-		const currentIndex = Math.floor(Math.random() * maxSize);
+		const currentIndex = Math.round(Math.random() * maxSize);
 		newArray.push(prevArray[currentIndex]);
 		prevArray = prevArray?.filter((item, index) => currentIndex !== index);
 	}
